98ff00d0f43ebfa5fba3eee6db582c7b624cda99
Mehdirben <rmehdib@gmail.com>
2024-11-27 01:32:35 +0100

    center widgets

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -48,121 +48,124 @@ class HomePage extends StatelessWidget {
           ),
         ],
       ),
-      body: Column(
-        children: [
-          // X-Ray Analysis Section
-          GestureDetector(
-            onTap: () {
-              // Navigate to the X-Ray Analysis Page
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => Scaffold(
-                    appBar: AppBar(
-                      title: Text(
-                        'X-Ray Analysis',
-                        style: GoogleFonts.lato(),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min, // Center widgets vertically
+          children: [
+            // X-Ray Analysis Section
+            GestureDetector(
+              onTap: () {
+                // Navigate to the X-Ray Analysis Page
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => Scaffold(
+                      appBar: AppBar(
+                        title: Text(
+                          'X-Ray Analysis',
+                          style: GoogleFonts.lato(),
+                        ),
+                        centerTitle: true,
+                        backgroundColor: Colors.blue[800],
                       ),
-                      centerTitle: true,
-                      backgroundColor: Colors.blue[800],
+                      body: const XRayPage(),
                     ),
-                    body: const XRayPage(),
                   ),
+                );
+              },
+              child: Container(
+                margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+                padding: const EdgeInsets.all(16.0),
+                width: double.infinity, // Make it take the full width
+                decoration: BoxDecoration(
+                  color: Colors.blue[100], // Light blue background for this section
+                  borderRadius: BorderRadius.circular(12),
                 ),
-              );
-            },
-            child: Container(
-              margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-              padding: const EdgeInsets.all(16.0),
-              width: double.infinity, // Make it take the full width
-              decoration: BoxDecoration(
-                color: Colors.blue[100], // Light blue background for this section
-                borderRadius: BorderRadius.circular(12),
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Icon(
-                    Icons.radio, // Icon for X-Ray Analysis
-                    size: 48,
-                    color: Colors.blue[800],
-                  ),
-                  const SizedBox(height: 8),
-                  Text(
-                    'X-Ray Analysis',
-                    style: GoogleFonts.lato(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.radio, // Icon for X-Ray Analysis
+                      size: 48,
                       color: Colors.blue[800],
                     ),
-                  ),
-                  const SizedBox(height: 4),
-                  Text(
-                    'AI-powered diagnostics for skeletal anomalies.',
-                    textAlign: TextAlign.center,
-                    style: GoogleFonts.lato(fontSize: 14, color: Colors.grey[700]),
-                  ),
-                ],
+                    const SizedBox(height: 8),
+                    Text(
+                      'X-Ray Analysis',
+                      style: GoogleFonts.lato(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blue[800],
+                      ),
+                    ),
+                    const SizedBox(height: 4),
+                    Text(
+                      'AI-powered diagnostics for skeletal anomalies.',
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.lato(fontSize: 14, color: Colors.grey[700]),
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-          // Medication Checker Section
-          GestureDetector(
-            onTap: () {
-              // Navigate to the Medication Checker Page
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => Scaffold(
-                    appBar: AppBar(
-                      title: Text(
-                        'Medication Checker',
-                        style: GoogleFonts.lato(),
+            // Medication Checker Section
+            GestureDetector(
+              onTap: () {
+                // Navigate to the Medication Checker Page
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => Scaffold(
+                      appBar: AppBar(
+                        title: Text(
+                          'Medication Checker',
+                          style: GoogleFonts.lato(),
+                        ),
+                        centerTitle: true,
+                        backgroundColor: Colors.green[800],
                       ),
-                      centerTitle: true,
-                      backgroundColor: Colors.green[800],
+                      body: const MedicationPage(),
                     ),
-                    body: const MedicationPage(),
                   ),
+                );
+              },
+              child: Container(
+                margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+                padding: const EdgeInsets.all(16.0),
+                width: double.infinity, // Make it take the full width
+                decoration: BoxDecoration(
+                  color: Colors.green[100], // Light green background for this section
+                  borderRadius: BorderRadius.circular(12),
                 ),
-              );
-            },
-            child: Container(
-              margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-              padding: const EdgeInsets.all(16.0),
-              width: double.infinity, // Make it take the full width
-              decoration: BoxDecoration(
-                color: Colors.green[100], // Light green background for this section
-                borderRadius: BorderRadius.circular(12),
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Icon(
-                    Icons.medication, // Icon for Medication Checker
-                    size: 48,
-                    color: Colors.green[800],
-                  ),
-                  const SizedBox(height: 8),
-                  Text(
-                    'Medication Checker',
-                    style: GoogleFonts.lato(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.medication, // Icon for Medication Checker
+                      size: 48,
                       color: Colors.green[800],
                     ),
-                  ),
-                  const SizedBox(height: 4),
-                  Text(
-                    'Analyze medication interactions and find alternatives.',
-                    textAlign: TextAlign.center,
-                    style: GoogleFonts.lato(fontSize: 14, color: Colors.grey[700]),
-                  ),
-                ],
+                    const SizedBox(height: 8),
+                    Text(
+                      'Medication Checker',
+                      style: GoogleFonts.lato(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.green[800],
+                      ),
+                    ),
+                    const SizedBox(height: 4),
+                    Text(
+                      'Analyze medication interactions and find alternatives.',
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.lato(fontSize: 14, color: Colors.grey[700]),
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
