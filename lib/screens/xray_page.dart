97b4dd631e5022763eb52d38e4286221873850c5
Mehdirben <rmehdib@gmail.com>
2024-11-30 21:15:38 +0100

    Firebase works

--- lib/screens/xray_page.dart
+++ lib/screens/xray_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'ViewReportsPage.dart';
+import 'viewreportspage.dart';
 
 class XRayPage extends StatelessWidget {
   const XRayPage({super.key});
